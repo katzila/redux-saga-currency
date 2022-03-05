2113eec1d1e0a4d146a7d2c6958bf521d2ff905c
Andrey Lyakhov <3qandre17@gmail.com>
2022-03-05 14:08:25 +0700

    feat: add footer, currencies list template

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react'
-import { Button, Layout, Space } from 'antd';
+import { Button, Layout, Typography, Row, Col, Space } from 'antd';
 
 import { getCurrency } from './app/reducers/currency';
+import { getCurrenciesList } from './app/reducers/currenciesList';
 import Navbar from './components/Navbar';
 import './App.css';
 
-function App() {
-  const dispatch = useDispatch();
+const { Title, Text } = Typography;
 
+function App() {
+  const dispatch1 = useDispatch();
+  const dispatch2 = useDispatch();
 
   const handleCurrency = () => {
-    dispatch(getCurrency())
+    dispatch1(getCurrency())
   }
 
-  useEffect(handleCurrency, [dispatch])
+  const handleCurrenciesList = () => {
+    dispatch2(getCurrenciesList())
+  }
+
+  useEffect(handleCurrency, [dispatch1])
+  useEffect(handleCurrenciesList, [dispatch2])
 
   const rate = useSelector((state) => state?.currencyReducer?.currency)
+  const currencies = useSelector((state) => state?.currenciesListReducer?.currencies)
 
   return (
     <div className="App">
       <Navbar />
       <Layout>
-        <h1>{`USD to RUB : ${rate}`}</h1>
+        <Title level={1}>{`USD to RUB : ${rate}`}</Title>
+        <Title level={2}>{currencies?.keys()}</Title>
         <Button type='primary' onClick={handleCurrency}>Get new currency rate</Button>
       </Layout>
+      <div className='footer'>
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+          Currency converter <br />
+          With useless footer
+        </Typography.Title>
+      </div>
     </div>
   );
 }
