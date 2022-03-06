33d15ad907ce9893379656f6e166737005068b10
Andrey Lyakhov <3qandre17@gmail.com>
2022-03-06 22:26:13 +0700

    chore: clening commented lines, unused vars

--- src/components/Currencies.js
+++ src/components/Currencies.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { Typography, Row, Select, Space, Col, Table } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -9,23 +9,21 @@ const { Title } = Typography;
 const { Option } = Select;
 
 const Currencies = () => {
-    const dispatch1 = useDispatch();
-    const dispatch2 = useDispatch();
+    const dispatchRates = useDispatch();
+    const dispatchList = useDispatch();
 
     const handleRates = (fromCurrency, toCurrencies) => {
-        dispatch1(getRates(fromCurrency, toCurrencies))
+        dispatchRates(getRates(fromCurrency, toCurrencies))
     }
 
     const handleCurrenciesList = () => {
-        dispatch2(getCurrenciesList())
+        dispatchList(getCurrenciesList())
     }
 
-    useEffect(handleCurrenciesList, [dispatch2])
+    useEffect(handleCurrenciesList, [dispatchList])
 
     const rates = useSelector((state) => state?.ratesReducer?.rates)
     const currencies = useSelector((state) => state?.currenciesListReducer?.currencies)
-
-    // const [currenciesData, setCurrenciesData] = useState([]);
 
     const columns = [
         {
@@ -45,15 +43,6 @@ const Currencies = () => {
             key !== value && key !== 'VEF'  //нерабочая валюта
         ))
         handleRates(value, filteredCurrencies);
-        // setCurrenciesData(
-        //     filteredCurrencies.map((key, index) => (
-        //         {
-        //             key: index,
-        //             currency: `${key}/${value}`,
-        //             rate: rates[index]
-        //         }
-        //     ))
-        // );
     }
 
     if (!currencies) return (<div>Loading</div>)
@@ -75,7 +64,6 @@ const Currencies = () => {
                             {Object.keys(currencies)?.map((key) => <Option value={key}>{`${currencies[key]}(${key})`}</Option>)}
                         </Select>
                     </Space>
-                    {/* <Title level={1}>{`USD to RUB : ${rates[0]}`}</Title> */}
                     <Table columns={columns} dataSource={rates} />
                 </Col>
             </Row>
