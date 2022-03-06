d652971285e35fbe9e2e19cb02f5458fe903d1d8
Andrey Lyakhov <3qandre17@gmail.com>
2022-03-06 22:12:52 +0700

    fix: navbar selection fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Menu } from 'antd'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 
 const Navbar = () => {
 
+  let location = useLocation()
+
   return (
     <div className='nav-container'>
-      <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['/']}>
+      <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['/']} selectedKeys={[location.pathname]}>
         <Menu.Item key='/'>
           <Link to='/'>
             Currencies
