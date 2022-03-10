422af2d65204d80c4e831f44f6ca19b75b5e57e3
Andrey Lyakhov <3qandre17@gmail.com>
2022-03-10 12:47:02 +0700

    chore: disable .StrictMode

--- src/index.js
+++ src/index.js
@@ -9,12 +9,10 @@ import store from './app/store';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
